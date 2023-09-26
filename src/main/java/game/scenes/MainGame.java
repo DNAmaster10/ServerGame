10f70b992e825d808927e0e050d45a2dd9b66f53
Louis <44494235+DNAmaster10@users.noreply.github.com>
2023-09-26 15:26:55 +0100

    Added comments
    Not finished yet, need to add more over the coming weeks

--- src/main/java/game/scenes/MainGame.java
+++ src/main/java/game/scenes/MainGame.java
@@ -31,8 +31,11 @@ public class MainGame {
     public static int packetRemovalInterval = 5;
     //Indicated the last time that packets where removed
     public static int lastPacketRemoval;
+    //The ammount of time to wait between generating a new building
     public static int generateInterval = 1;
+    //The last time that a building was generated
     public static int lastGenerate;
+    //Various hash maps containing different game objects
     public static HashMap<Integer, Router> routers = new HashMap<>();
     public static HashMap<Integer, Cable> cables = new HashMap<>();
     public static HashMap<String, Cable> cablesIdMap = new HashMap<>();
@@ -41,54 +44,69 @@ public class MainGame {
     public static HashMap<Integer, Structure> buildings = new HashMap<>();
     public static HashMap<Integer, Chunk> chunks = new HashMap<>();
     public static HashMap<Integer, Structure> structures = new HashMap<>();
+    //A list of all packets currently travelling through the network
     public static List<Packet> packets = new ArrayList<>();
+    //The node ID of the player's HQ, used later for ensuring that a given building is connected to a destination node
     public static int HQId;
 
     public static class grid {
+        //Diameters of a cell in window measurements
         public static int cellWindowWidth = 10;
         public static int cellWindowHeight = 10;
 
+        //Diameters of a chunk in cell measurements
         public static int chunkWidth = 10;
         public static int chunkHeight = 10;
     }
     public static Structure getStructureByGridPos(int gridX, int gridY) {
+        //Gets a structure from grid position
         for (Structure structure : structures.values()) {
             if (structure.gridX == gridX && structure.gridY == gridY) {
                 return(structure);
             }
         }
+        //Return null if no structure exists in the hash map
         return null;
     }
     public static Structure getStructureById(int id) {
+        //Returns a structure by ID
         if (structures.containsKey(id)) {
             return(structures.get(id));
         }
+        //Returns null if no structure exists with the given ID
         return(null);
     }
     public static boolean checkSolidByGridPos(int gridX, int gridY) {
         //Returns a boolean indicating whether a router OR building is present
         for (Structure structure : structures.values()) {
             if (structure.gridX == gridX && structure.gridY == gridY) {
+                //Returne true here if the given position matches that of the found building
                 return(true);
             }
         }
+        //Returns false if neither a building nor a router is present
         return false;
     }
     public static boolean checkRouterById(int id) {
+        //Returns a boolean indicating whether or not a router with the given ID exists in the hash table
         return routers.containsKey(id);
     }
     public static boolean checkChunkByChunkPos(int chunkX, int chunkY) {
+        //Checks whether a chunk exists at a chiven chunk position (Note that this is different from a cell position, chunks contains multiple cells)
         for (Chunk chunk : chunks.values()) {
             if (chunk.chunkX == chunkX && chunk.chunkY == chunkY) {
                 return true;
             }
         }
+        //Returns false if no chunk has been generated at the given position 
         return false;
     }
     public static boolean checkCableByStructureIds(int sourceStructureId, int destStructureId) {
+        //Returns a boolean if a cable / edge exists between two given structures (e.g a house and a router)
         return(NodeGraph.checkEdge(sourceStructureId, destStructureId));
     }
     public static Cable getCableByStructureIds(int sourceStructureId, int destStructureId) {
+        //Returns a cable if one runs between the two given nodes. If no cable or edge exists, return Null
         String key = sourceStructureId + "," + destStructureId;
         if (cablesIdMap.containsKey(key)) {
             return(cablesIdMap.get(key));
@@ -96,16 +114,20 @@ public class MainGame {
         return null;
     }
     public static Server getServerById(int id) {
+        //Returns a server by the given ID
         if (servers.containsKey(id)) {
             return(servers.get(id));
         }
+        //Return null if no server exists
         return null;
     }
 
     public static void init(int gridWidth, int gridHeight) {
+        //Initializes the game
         Player.init();
         Gui.init();
         InitChunks.init();
+        //A chunk must first be created to kickstart chunk generation
         SpawnChunk firstChunk = new SpawnChunk(0, 0);
         firstChunk.generateFull();
         chunks.put(firstChunk.id, firstChunk);
@@ -116,36 +138,40 @@ public class MainGame {
         List<Chunk> notFullChunks = new ArrayList<>();
         for (Chunk chunk : chunks.values()) {
             if (!chunk.full) {
+                //If there is a chunk which is not full, add it to the list of chunks which can be generated
                 notFullChunks.add(chunk);
             }
         }
         if (notFullChunks.size() > 0) {
             int selectedChunk;
-            //Out of the chunks which are not full, pick a random one, or the only one
-            //And generate one
+            //Out of the chunks which are not full, pick a random one, or the only one, and add a building to that chunk
+            //If all chunks are full, a new chunk will be generated later
             if (notFullChunks.size() == 1) {
                 selectedChunk = 0;
             }
             else {
                 selectedChunk = ThreadLocalRandom.current().nextInt(0, notFullChunks.size());
             }
+            //Exit if a chunk had a building added to it
             notFullChunks.get(selectedChunk).generateOne();
             return;
         }
         //If not, then create a new chunk
-        //First, for all the chunks with an empty border, get them
+        //First, all the chunks with an empty border must be selected
         List<Chunk> emptyBorderChunks = new ArrayList<>();
         for (Chunk chunk : chunks.values()) {
             if (!chunk.bordersFull) {
                 emptyBorderChunks.add(chunk);
             }
         }
-        //Out of these chunks, pick a random one
+        //Out of these chunks...
         int emptyBorderChunkIndex;
         if (emptyBorderChunks.size() == 1) {
+            //Select the only existing one if only 1 chunk has an empty border
             emptyBorderChunkIndex = 0;
         }
         else {
+            //or select a random one if there are multiple
             emptyBorderChunkIndex = ThreadLocalRandom.current().nextInt(0, emptyBorderChunks.size());
         }
         Chunk emptyBorderChunk = emptyBorderChunks.get(emptyBorderChunkIndex);
@@ -163,18 +189,23 @@ public class MainGame {
         if (!checkChunkByChunkPos(emptyBorderChunk.chunkX - 1, emptyBorderChunk.chunkY)) {
             emptyBorders.add(3);
         }
-        //Now, select a random border
+        //Now, select a random empty border
         int chunkBorder;
         if (emptyBorders.size() == 0) {
+            //If the chunk had no empty borders, we can set it to "full", since there was likely an error somewhere. We can return and the chunk will be skipped next time.
+            //It's unlikely that this branch will ever be executed, but it acts as a failsafe.
             emptyBorderChunk.bordersFull = true;
             return;
         }
         if (emptyBorders.size() == 1) {
+            //Set the chunk border to the only integer in emptyBorders, since there is only one
             chunkBorder = emptyBorders.get(0);
         }
         else {
+            //Pick a random border out of the empty borders around the chunk
             chunkBorder = emptyBorders.get(ThreadLocalRandom.current().nextInt(0, emptyBorders.size()));
         }
+        //Used as a 2D position vector
         int[] chunkPosition = new int[2];
         //Now based on the selected border, get the position of the new chunk
         switch(chunkBorder) {
@@ -197,6 +228,7 @@ public class MainGame {
         }
         //Now, generate the chunk in the position
         int randomChunk;
+        //Select a chunk type to generate
         randomChunk = ThreadLocalRandom.current().nextInt(1, 4);
         //INSERT CODE TO GEN RANDOM CHUNK HERE
 
