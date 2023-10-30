546d406d290949de6d2d4c5f2ee0e2232647d820
Louis <44494235+DNAmaster10@users.noreply.github.com>
2023-10-30 09:34:30 +0000

    Update Packets.java

--- src/main/java/game/scenes/maingame/Packets.java
+++ src/main/java/game/scenes/maingame/Packets.java
@@ -8,8 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Packets {
-    //Calculated on edge creation
+    //Calculated on edge creation, is a list of all servers connected to the HQ building
     public static List<Integer> availableServers = new ArrayList<>();
+    //Calculated on edge creation, is a list of all consumers connected to the HQ
     public static List<Integer> availableConsumers = new ArrayList<>();
     public static void tickPackets() {
         //Create packets where needed
